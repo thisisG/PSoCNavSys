722ecd99caf2e0c4717d36becbaab1548aef8b30
thisisG <geir.istad.11@aberdeen.ac.uk>
2015-01-23 18:32:30 +0000

    Added a NavTester project for better clarity and differentiation between the (messy) development code and the testing facilities.

---
+++ WaypointSystem/NavTester/NavTester.cpp
@@ -0,0 +1,26 @@
+// NavTester.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <iostream>
+#include "./../WaypointSystem/csv.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include "./../WaypointSystem/navfunctions.h"
+#include "./../WaypointSystem/navtypes.h"
+#include "./../WaypointSystem/nmea/nmea.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
