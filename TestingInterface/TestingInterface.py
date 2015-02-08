250e2781b83722ab2e79c17bfd2de2e262a7a0f0
thisisG <geir.istad.11@aberdeen.ac.uk>
2015-02-08 17:56:14 +0000

    Added Python script for logging data sent from PSoC when testing

--- TestingInterface/TestingInterface.py
+++ TestingInterface/TestingInterface.py
@@ -1,10 +1,13 @@
 __author__ = 'Geir'
 
 import serial
-import io
 
 
 class TestInterface:
+    psoc_serial = serial.Serial()
+    csv_file_name = "default.csv"
+    line_buffer = ""
+
     def __init__(self):
         """
         Add an init method at a later time
@@ -12,6 +15,22 @@ class TestInterface:
         """
         self.psoc_serial.baudrate = 9600
         self.psoc_serial.bytesize = 8
-        self.psoc_serial.parity = 'None'
+        self.psoc_serial.parity = 'N'
 
-    psoc_serial = serial.Serial()
+    def set_file_name(self, filename):
+        self.csv_file_name = filename
+
+    def start_logging_data(self):
+        if not self.psoc_serial.isOpen():
+            self.psoc_serial.open()
+        with open(self.csv_file_name, 'w', newline='\n') as self.csv_file:
+            print("Opening serial port and reading data from PSoC")
+            while True:
+                self.line_buffer = self.psoc_serial.readline()
+                print(self.line_buffer)
+                self.csv_file.write(self.line_buffer)
+
+
+myTest = TestInterface()
+
+myTest.start_logging_data()
