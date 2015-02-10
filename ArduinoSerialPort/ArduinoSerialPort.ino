9cb6edcfb4c6810b85c082bc516c63faaa0e5845
thisisG <geir.istad.11@aberdeen.ac.uk>
2015-02-10 21:38:48 +0000

    Added serial echoing for Arduino serial communication.

--- ArduinoSerialPort/ArduinoSerialPort.ino
+++ ArduinoSerialPort/ArduinoSerialPort.ino
@@ -1,20 +1,39 @@
 #include <Arduino.h>
 
+HardwareSerial PCSerial;
+HardwareSerial PSoCSerial;
+
+static const boolean echo = false;
+
 void setup()
 {
-    Serial.begin(9600);
-    Serial1.begin(9600);
-    /* add setup code here */
+    PCSerial.begin(9600);
+    PSoCSerial.begin(9600);
 }
-
-int counter = 0;
 
 void loop()
 {
-    if (counter <= 2)
+    static int counter = 0;
+    static char charFromPC = '\0';
+    static char charFromPSoC = '\0';
+
+    if (echo)
     {
-        Serial.println("a");
-        Serial1.println("b");
+        if (PCSerial.available() > 0)
+        {
+            charFromPC = PCSerial.read();
+            PSoCSerial.print(charFromPC);
+        }
+        if (PSoCSerial.available() > 0)
+        {
+            charFromPSoC = PSoCSerial.read();
+            PCSerial.print(charFromPSoC);
+        }
+    }
+    else if (counter <= 2)
+    {
+        PCSerial.println("a");
+        PSoCSerial.println("b");
         delay(200);
         counter++;
     }
