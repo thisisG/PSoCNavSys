a64a39529fa3787a6b51252ca152415ead0ffb77
Geir Istad <geir.istad.11@aberdeen.ac.uk>
2015-04-18 19:09:03 +0200

    Updated data from nav test. Changed matlab code.
    Removed duplicate positions from the start of the navigation tests in
    car.
    
    Updated MATLAB code used to process data from simulations and tests to
    increase the decimal point precision for the output of the processed
    data.

--- MatlabNavTests/plotNavPath.m
+++ MatlabNavTests/plotNavPath.m
@@ -7,23 +7,25 @@ plot(wpLon,wpLat, 'LineStyle', 'none', 'Marker', 'x', 'MarkerSize', 12,...
 %'MarkerEdgeColo', 'blue')
 %plot(Ex2Lon,Ex2Lat, 'LineStyle', 'none', 'Marker', 'o', 'MarkerSize', 12,...
 %'MarkerEdgeColo', 'black')
-r = 0.025;
+r = 0.0015;
 dHeading = heading+270;
 dHeading = mod(dHeading,360);
 dHeading = 360-dHeading;
 rHeading = (pi*dHeading)/(180)
-n = 10
-arrowLat = curLat(2 : n : end);
-arrowLon = curLon(2 : n : end);
+n = 39
+arrowLat = curLat(1 : n : end);
+arrowLon = curLon(1 : n : end);
 arrowHeading = dHeading(2 : n : end);
-%%
+
 
 u = r*cosd(arrowHeading)
 v = r*sind(arrowHeading)
 
-%%
+
 h = quiver(arrowLon, arrowLat,u,v,1)
 %%
 arrows = horzcat(arrowLon, arrowLat, u, v)
 
-csvwrite('NavTestTwoAQuiver', arrows)
+dlmwrite('ProperTestQuiver.csv', arrows, 'precision', 10)
+
+%csvwrite('ProperTestQuiver.csv', arrows)
